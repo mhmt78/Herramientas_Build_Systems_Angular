9a53e31f94b5cebe7727d776509ac9f50bcef7bb
mhmt78 <mhernan157@hotmail.com>
2020-04-20 21:20:54 -0500

    haber como nos sale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './Components/login/login.component';
+import { InicioComponent } from './Components/inicio/inicio.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'haber';
+  title = 'app works!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { Angular2RoutingModule } from './app.routing.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { InicioComponent } from './Components/inicio/inicio.component';
+import { NavegacionComponent } from './Components/navegacion/navegacion.component';
+import { CatalogoComponent } from './Components/catalogo/catalogo.component';
+
+import { DataService } from './services/data.service';
+import { AuthService } from './services/auth.service';
+import { DetalleComponent } from './Components/detalle/detalle.component';
+import { VerProductoComponent } from './Components/ver-producto/ver-producto.component';
+import { CarritoComponent } from './Components/carrito/carrito.component';
+import { DetalleCarritoComponent } from './Components/detalle-carrito/detalle-carrito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InicioComponent,
+    NavegacionComponent,
+    CatalogoComponent,
+    DetalleComponent,
+    VerProductoComponent,
+    CarritoComponent,
+    DetalleCarritoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    Angular2RoutingModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [DataService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
